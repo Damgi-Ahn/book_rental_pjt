48678a1f5af68abee765096ce9abdbd37897649e
Damgi Ahn <damgi.dev@gmail.com>
2024-11-30 17:27:56 +0900

    ver_2 commit

---
+++ src/main/java/com/office/library/bootrentalsystem/BootRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.office.library.bootrentalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRentalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRentalSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/office/library/bootrentalsystem/BootRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.office.library.bootrentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRentalSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
